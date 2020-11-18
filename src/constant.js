b0a6522a510607371f3e85aba356d3aadda1f138
hidnarola <hda@narola.email>
2020-11-18 11:01:07 +0530

    Latest changes with leaflet

--- src/constant.js
+++
@@ -1,3 +0,0 @@
-//ssi create API_KEY_1
-// export const API_KEY_1 ="AIzaSyAcuS0J-U4vpf1TYy20C3seKvOfu2KrmNY"
-export const API_KEY ="AIzaSyBWIIb5rlIPoxCKRCQlueENTkj2KcniU1I"
--- src/views/common/Search/index.vue
+++ src/views/common/Search/index.vue
@@ -1,420 +1,113 @@
 <template>
-  <section id="playground">
-    <h4>Search</h4>
-
-    <div class="separter"></div>
-    <div style="border: 1px solid grey; padding: 25px 10px; border-radius: 5px">
-      <div class="col-sm-12">
-        <div class="row">
-          <div class="col-sm-8">
-            <div class="map-box">
-              <button @click="deleteSelectedShape">Remove Shape</button>
-              <input
-                id="pac-input"
-                @keyup="value = $event.target.value"
-                class="controls serach-input"
-                type="text"
-                placeholder="Search Box"
-              />
-              <div id="map"></div>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div id="cropped_Img">
-              <img src id="preview_image" />
-            </div>
-            <div class="text-center">
-              <b-button
-                v-b-modal.modal-1
-                class="btn crop-btn"
-                :disabled="value === null"
-                @click="openModal"
-                >Crop</b-button
-              >
-              <b-modal id="modal-1" title @ok="handleOk">
-                <p class="my-4" v-if="value === null || value === ''">
-                  Please Enter your address in search Box !
-                </p>
-                <p class="my-4" v-else>
-                  Are you sure to take capture this location ?
-                </p>
-              </b-modal>
-            </div>
-            <div id="panel">
-              <div id="color-palette"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <div>
+    <div id="map"></div>
+  </div>
 </template>
 
 <script>
-import fxDebounceInput from "@/components/frogx-ui/debounceInput/index";
-import { API_KEY } from "../../../constant";
-
-var polyOptions = {
-  strokeWeight: 0,
-  fillOpacity: 0.2,
-  editable: true,
-};
-var selectedShape;
-var drawingManager;
-
-var colors = ["#1E90FF", "#FF1493", "#32CD32", "#FF8C00", "#4B0082"];
-var selectedColor;
-var colorButtons = {};
 
 export default {
-  name: "Search",
-  components: { fxDebounceInput },
-
-  data() {
-    return {
-      isCrop: false,
-      value: null,
-      isOpenModal: false,
-      zoomMap: "19",
-      mapSize: "1000x500",
-      lat: null,
-      lng: null,
-    };
-  },
+  name: "LeafletMap",
   mounted() {
-    this.initAutocomplete();
+    this.initMap();
   },
   methods: {
-    makeEnableCrop() {
-      this.isCrop = !this.isCrop;
-    },
-    openModal() {
-      this.isOpenModal = !this.isOpenModal;
-    },
-    createMapImage({ lat, lng }) {
-      let _this = this;
-      var url = `https://maps.googleapis.com/maps/api/staticmap?zoom=${_this.zoomMap}&size=${_this.mapSize}&maptype=satellite&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${API_KEY}`;
-      document.getElementById("preview_image").src = url;
-    },
+    initMap() {
+      //simple map
+      var map = L.map("map").setView([-41.2858, 174.78682], 15);
 
-    handleOk(bvModalEvt) {
-      if (this.value !== null || this.value !== " ") {
-        bvModalEvt.preventDefault();
-        var address = this.value;
-        console.log("address => ", address);
-        let geocoder = new window.google.maps.Geocoder();
-        console.log("geocoder => ", geocoder);
-
-        let _this = this;
-        geocoder.geocode({ address: address }, (results) => {
-          console.log("results => ", results);
-
-          _this.lat = results[0] && results[0].geometry.location.lat();
-          _this.lng = results[0] && results[0].geometry.location.lng();
-
-          _this.createMapImage({ lat: _this.lat, lng: _this.lng });
-        });
-        this.$nextTick(() => {
-          this.$bvModal.hide("modal-1");
-        });
-      }
-    },
-
-    setSelection(shape) {
-      console.log("sh shape => ", shape.latLngs.getPath());
-
-      let _this = this;
-      _this.clearSelection();
-
-      selectedShape = shape;
-      shape.setEditable(true);
-      _this.selectColor(shape.get("fillColor") || shape.get("strokeColor"));
-    },
-
-    clearSelection() {
-      if (selectedShape) {
-        selectedShape.setEditable(false);
-        selectedShape = null;
-      }
-    },
-
-    deleteSelectedShape() {
-      if (selectedShape) {
-        console.log("selectedShape => ", selectedShape);
-        console.log("selectedShape.setMap => ", selectedShape.setMap);
-        selectedShape.setMap(null);
-      }
-    },
-
-    selectColor(color) {
-      selectedColor = color;
-      for (var i = 0; i < colors.length; ++i) {
-        var currColor = colors[i];
-        colorButtons[currColor].style.border =
-          currColor == color ? "2px solid #789" : "2px solid #fff";
-      }
-
-      // Retrieves the current options from the drawing manager and replaces the
-      // stroke or fill color as appropriate.
-      var polylineOptions = drawingManager.get("polylineOptions");
-      polylineOptions.strokeColor = color;
-      drawingManager.set("polylineOptions", polylineOptions);
-
-      var rectangleOptions = drawingManager.get("rectangleOptions");
-      rectangleOptions.fillColor = color;
-      drawingManager.set("rectangleOptions", rectangleOptions);
-
-      var circleOptions = drawingManager.get("circleOptions");
-      circleOptions.fillColor = color;
-      drawingManager.set("circleOptions", circleOptions);
-
-      var polygonOptions = drawingManager.get("polygonOptions");
-      polygonOptions.fillColor = color;
-      drawingManager.set("polygonOptions", polygonOptions);
-    },
-
-    setSelectedShapeColor(color) {
-      if (selectedShape) {
-        if (selectedShape.type == google.maps.drawing.OverlayType.POLYLINE) {
-          selectedShape.set("strokeColor", color);
-        } else {
-          selectedShape.set("fillColor", color);
+      L.tileLayer(
+        "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        {
+          maxZoom: 21
         }
-      }
-    },
+      ).addTo(map);
 
-    makeColorButton(color) {
-      let _this = this;
-      var button = document.createElement("span");
-      button.className = "color-button";
-      button.style.backgroundColor = color;
-      google.maps.event.addDomListener(button, "click", function () {
-        _this.selectColor(color);
-        _this.setSelectedShapeColor(color);
+      var cost_underground = 12.55,
+        cost_above_ground = 17.89,
+        html = [
+          "<table>",
+          ' <tr><td class="cost_label">Cost Above Ground:</td><td class="cost_value">${total_above_ground}</td></tr>',
+          ' <tr><td class="cost_label">Cost Underground:</td><td class="cost_value">${total_underground}</td></tr>',
+          "</table>",
+        ].join(""),
+        numberWithCommas = function (x) {
+          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        };
+
+
+      var Ruler = L.Control.LinearMeasurement.extend({
+        // layerSelected: function (e) {
+        //   /* cost should be in feet */
+
+        //   var distance = e.total.scalar;
+
+        //   if (e.total.unit === "mi") {
+        //     distance *= e.sub_unit;
+        //   } else if (e.total.unit === "km") {
+        //     distance *= 3280.84;
+        //   } else if (e.total.unit === "m") {
+        //     distance *= 3.28084;
+        //   }
+
+        //   var data = {
+        //     total_above_ground: numberWithCommas(
+        //       L.Util.formatNum(cost_above_ground * distance, 2)
+        //     ),
+        //     total_underground: numberWithCommas(
+        //       L.Util.formatNum(cost_underground * distance, 2)
+        //     ),
+        //   };
+
+        //   var content = L.Util.template(html, data),
+        //     popup = L.popup().setContent(content);
+
+        //   e.total_label.bindPopup(popup, { offset: [45, 0] });
+        //   e.total_label.openPopup();
+        // },
       });
 
-      return button;
-    },
 
-    buildColorPalette() {
-      let _this = this;
-      var colorPalette = document.getElementById("color-palette");
-      for (var i = 0; i < colors.length; ++i) {
-        var currColor = colors[i];
-        var colorButton = _this.makeColorButton(currColor);
-        colorPalette.appendChild(colorButton);
-        colorButtons[currColor] = colorButton;
-      }
-      _this.selectColor(colors[0]);
-    },
-
-    initAutocomplete() {
-      let _this = this;
-
-      let map =
-        window.google &&
-        new window.google.maps.Map(document.getElementById("map"), {
-          center: {
-            lat: -33.8688,
-            lng: 151.2195,
-          },
-          zoom: 22,
-          mapTypeId: "satellite",
-        }); // Create the search box and link it to the UI element.
-
-      const input = document.getElementById("pac-input");
-
-      const searchBox =
-        window.google && new window.google.maps.places.SearchBox(input);
-
-      map &&
-        map.controls[
-          window.google && window.google.maps.ControlPosition.TOP_LEFT
-        ].push(input); // Bias the SearchBox results towards current map's viewport.
-
-      map &&
-        map.addListener("bounds_changed", () => {
-          searchBox.setBounds(map.getBounds());
-        });
-      let markers = []; // Listen for the event fired when the user selects a prediction and retrieve
-      // more details for that place.
-
-      searchBox &&
-        searchBox.addListener("places_changed", () => {
-          const places = searchBox.getPlaces();
-          if (places.length == 0) {
-            return;
-          } // Clear out the old markers.
-
-          markers.forEach((marker) => {
-            marker.setMap(null);
-          });
-          markers = []; // For each place, get the icon, name and location.
-          var markers2 = []; // For each place, get the icon, name and location.
-
-          const bounds = window.google && new window.google.maps.LatLngBounds();
-
-          places.forEach((place) => {
-            if (!place.geometry) {
-              console.log("Returned p0lace contains no geometry");
-              return;
-            }
-
-            markers.push(
-              window.google &&
-                new window.google.maps.Marker({
-                  draggable: true,
-                  map,
-                  title: place.name,
-                  position: place.geometry.location,
-                })
-            );
-
-            markers.forEach((marker) => {
-              google.maps.event.addListener(marker, "dragend", (event) => {
-                _this.lng = event.latLng.lng();
-                _this.lat = event.latLng.lat();
-                _this.createMapImage({ lat: _this.lat, lng: _this.lng });
-              });
-            });
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-
-      // Creates a drawing manager attached to the map that allows the user to draw
-      // markers, lines, and shapes.
-
-      drawingManager = new google.maps.drawing.DrawingManager({
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            // google.maps.drawing.OverlayType.MARKER,
-            // google.maps.drawing.OverlayType.CIRCLE,
-            // google.maps.drawing.OverlayType.POLYGON,
-            google.maps.drawing.OverlayType.POLYLINE,
-            // google.maps.drawing.OverlayType.RECTANGLE,
-          ],
-        },
-        markerOptions: {
-          draggable: true,
-        },
-        polylineOptions: {
-          editable: true,
-        },
-        // rectangleOptions: polyOptions,
-        // circleOptions: polyOptions,
-        // polygonOptions: polyOptions,
-        map: map,
-      });
-
-      // testing code
-      //     google.maps.event.addListener(
-      //       drawingManager,
-      //       "polylinecomplete",
-      //       function (polyline) {
-      //         console.log("polyline => ", polyline);
-
-      //         var pointxy = polyline.getPath();
-
-      //         console.log("pointxy => ", pointxy);
-
-      //         var triangleCoords = [];
-      //         for (var i = 0; i < pointxy.length; i++) {
-      //           console.log('pointxy.getAt(i).lat() => ',pointxy.getAt(i).lat());
-
-      //           var pointstemp = new google.maps.LatLng( pointxy.getAt(i).lat(), pointxy.getAt(i).lng() );
-      // console.log('pointstemp. => '+i ,pointstemp.lat());
-      // console.log('pointstemp. => '+i ,pointstemp.lng());
-
-      //           triangleCoords.push(pointstemp);
-      //           // console.log('triangleCoords => ',triangleCoords);
-      //           // console.log('triangleCoords => ',triangleCoords[0].lat(), triangleCoords[0].lng());
-      //           // console.log('triangleCoords => ',triangleCoords && triangleCoords[1].lat(), triangleCoords && triangleCoords[1].lng());
-      //         }
-      //         console.log('triangleCoords => ',triangleCoords);
-
-      //         //Call the API interface directly to calculate the length and unit meters of several line segments
-      //         var distanceValue = google.maps.geometry.spherical.computeLength( triangleCoords );
-
-      //         console.log("distanceValue =>", distanceValue);
-
-      //         // distanceValue = (distanceValue/1000).toFixed(2);
-      //         // distanceValue += "Km";
-
-      //         // console.log('distanceValue after=> ',distanceValue);
-
-      //         var marker = new google.maps.Marker({
-      //           map: map,
-      //           position: triangleCoords[triangleCoords.length - 1],
-      //           label: distanceValue,
-      //         });
-      //       }
-      //     );
-
-      // testing code end
-
-      google.maps.event.addListener(drawingManager, "polylinecomplete", (e) => {
-        console.log("event ii => ", e);
-        var pointxy = e.getPath();
-        console.log("pointxy iii => ", pointxy);
-
-        var triangleCoords = [];
-        for (var i = 0; i < pointxy.length; i++) {
-          console.log("pointxy.getAt(i).lat() => ", pointxy.getAt(i).lat());
-
-          var pointstemp = new google.maps.LatLng( pointxy.getAt(i).lat(), pointxy.getAt(i).lng() );
-          console.log("pointstemp. iii => " + i, pointstemp.lat());
-          console.log("pointstemp. iii => " + i, pointstemp.lng());
-
-          triangleCoords.push(pointstemp);
-          console.log("triangleCoords iii => ", triangleCoords);
-        }
-
-        var distanceValue = google.maps.geometry.spherical.computeLength( triangleCoords );
-
-        console.log("distanceValue =>", distanceValue);
-
-        if (e.type != google.maps.drawing.OverlayType.MARKER) {
-          // Switch back to non-drawing mode after drawing a shape.
-          drawingManager.setDrawingMode(null);
-          // Add an event listener that selects the newly-drawn shape when the user
-          // mouses down on it.
-          var newShape = e.overlay;
-          newShape.type = e.type;
-
-          google.maps.event.addListener(newShape, "click", () => {
-            console.log("newShape => ", newShape);
-            _this.setSelection(newShape);
-            console.log(
-              "_this.setSelection(newShape)=> ",
-              _this.setSelection(newShape)
-            );
-          });
-          _this.setSelection(newShape);
-        }
-      });
-
-      // google.maps.event.addListener(
-      //   drawingManager,
-      //   "polylinecomplete",
-      //   function (event) {
-      //     console.log("Heyyyyyyyyyyy getPath", event.getPath());
-      //     console.log("Heyyyyyyyyyyy getArray", event.getPath().getArray());
-      //     console.log("Heyyyyyyyyyyy lat of [0]", event.getPath().getArray()[0].lat(), event.getPath().getArray()[0].lng());
-      //     console.log("Heyyyyyyyyyyy lat of [1]", event.getPath().getArray()[1].lat(), event.getPath().getArray()[1].lng());
-      //   }
-      // );
-
-      _this.buildColorPalette();
+      map.addControl(new Ruler({
+          unitSystem: "metric",
+          color: "#1e0fff",
+          opacity:0,
+          dashArray: [0,0],
+          dashArrayOptions:[]
+        })
+      );
     },
   },
 };
 </script>
+<style>
+body {
+  /* padding: 50px; */
+}
+
+h1 {
+  font-weight: 300;
+  font-size: 2em;
+  margin-top: -0.75em;
+  padding: 0;
+  color: #777;
+  text-align: center;
+  font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+}
+
+#map {
+  height: 90vh;
+}
+
+.cost_label {
+  font-weight: bold;
+}
+
+.cost_value {
+  text-align: left;
+}
+
+</style>
